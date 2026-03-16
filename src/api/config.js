d7040b01d785a48901b73e848832d7f2716ba240
Your Name <pandeyangel986@gmail.com>
2026-03-16 21:54:34 +0545

    updated

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "https://ecommerce-backend-aw9bk8qo0-anjal-pandeys-projects.vercel.app";
+const VITE_API_URL = import.meta.env.VITE_API_URL || "https://ecommerce-backend-hd6eo47c4-anjal-pandeys-projects.vercel.app";
+const API_URL = VITE_API_URL.replace(/\/$/, "");
 export default API_URL;
+
